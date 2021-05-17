c6d6f81c472d950a9177e0e82272ca165d45443a
Trey Pendragon <tpendragon@princeton.edu>
2021-05-17 16:28:36 -0700

    Lux.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,23 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import Vue from "vue/dist/vue.esm";
+import system from "lux-design-system";
+import "lux-design-system/dist/system/system.css";
+import "lux-design-system/dist/system/tokens/tokens.scss";
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+Vue.use(system)
+// create the LUX app and mount it to wrappers with class="lux"
+document.addEventListener("DOMContentLoaded", () => {
+  var elements = document.getElementsByClassName("lux")
+  for (var i = 0; i < elements.length; i++) {
+    new Vue({
+      el: elements[i]
+    })
+  }
+})
