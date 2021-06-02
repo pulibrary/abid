a59d1fd9b94a0a546e20fa76e77def22d01c67a5
Eliot Jordan <eliotj@princeton.edu>
2021-06-02 12:03:07 -0500

    Add specs for CAS integration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:access_token) { OmniAuth::AuthHash.new(provider: "cas", uid: "user") }
+
+  before do
+    FactoryBot.create(:user, uid: "user", provider: "cas")
+  end
+
+  describe "#from_cas" do
+    it "returns a user object" do
+      expect(described_class.from_cas(access_token)).to be_a described_class
+    end
+  end
 end
