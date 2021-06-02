eb8c000345854fb8401644aba17e04002ecc4f7d
Trey Pendragon <tpendragon@princeton.edu>
2021-06-02 12:55:06 -0700

    Aspace Setup

--- app/services/aspace/client.rb
+++ app/services/aspace/client.rb
@@ -1,4 +1,20 @@
 module Aspace
-  class Client
+  class Client < ArchivesSpace::Client
+    def self.config
+      ArchivesSpace::Configuration.new(
+        {
+          base_uri: Abid.config["archivespace_url"],
+          username: Abid.config["archivespace_user"],
+          password: Abid.config["archivespace_password"],
+          page_size: 50,
+          throttle: 0
+        }
+      )
+    end
+
+    def initialize
+      super(self.class.config)
+      login
+    end
   end
 end
---
+++ config/initializers/abid_config.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module Abid
+  def config
+    @config ||= config_yaml.with_indifferent_access
+  end
+
+  private
+
+  def config_yaml
+    YAML.safe_load(yaml, aliases: true)[Rails.env]
+  end
+
+  def yaml
+    ERB.new(File.read(Rails.root.join("config", "config.yml"))).result
+  end
+
+  module_function :config, :config_yaml, :yaml
+end
