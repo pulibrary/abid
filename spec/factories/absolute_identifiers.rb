172fbed46be1825a4acc2745abf1274cef7896cc
Trey Pendragon <tpendragon@princeton.edu>
2021-06-03 11:42:23 -0700

    Fix original box #

--- spec/factories/absolute_identifiers.rb
+++ spec/factories/absolute_identifiers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :absolute_identifier do
-    original_box_number { 1 }
+    original_box_number { 31 }
     # This is box 31 in ABID001
     top_container_uri { "/repositories/4/top_containers/118271" }
     prefix { "S" }
