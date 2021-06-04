1e1a001838336487fdfa37f0756582f334a5db11
Trey Pendragon <tpendragon@princeton.edu>
2021-06-04 11:17:14 -0700

    Add test for error state.

--- spec/system/batches_spec.rb
+++ spec/system/batches_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe "Batch management" do
 
     expect(page).to have_content "B-000001"
   end
+  it "displays errors if something is wrong" do
+    stub_top_container_search(ead_id: "ABID001", repository_id: "4", indicators: 40..41)
+    visit "/"
+    fill_in "First barcode", with: "32101113342909"
+    fill_in "Call number", with: "ABID001"
+    fill_in "Start box", with: 40
+    fill_in "End box", with: 41
+    select "Firestone Library, Vault, Manuscripts [mss]", from: "Location"
+    select "Standard manuscript", from: "Container profile"
+    click_button "Create Batch"
+
+    expect(page).to have_content "Unable to find matching top containers for all boxes"
+  end
 end
