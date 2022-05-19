1608d894a756d6907e4f1880ba53e4ff45a67ecd
Anna Headley <anna.headley@gmail.com>
2022-05-19 11:19:35 -0400

    bin/rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_210524) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_150752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +21,8 @@ ActiveRecord::Schema.define(version: 2021_09_02_210524) do
     t.integer "suffix"
     t.string "sync_status"
     t.string "pool_identifier"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "barcode"
     t.bigint "batch_id"
     t.string "batch_type", default: "Batch"
@@ -40,8 +39,8 @@ ActiveRecord::Schema.define(version: 2021_09_02_210524) do
     t.string "call_number"
     t.string "location_uri"
     t.string "container_profile_uri"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "resource_uri"
     t.bigint "user_id"
     t.jsonb "location_data"
@@ -51,18 +50,18 @@ ActiveRecord::Schema.define(version: 2021_09_02_210524) do
   end
 
   create_table "marc_batches", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_marc_batches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "remember_created_at"
+    t.datetime "remember_created_at", precision: nil
     t.string "provider", default: "cas", null: false
     t.string "uid", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "aspace_uri"
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
