3fb2a83bd781c2a13bf85c3de1e6298dc7a50416
Anna Headley <anna.headley@gmail.com>
2022-05-19 14:42:48 -0400

    Move javascript imports to import map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,46 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Turbolinks from "turbolinks"
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+import TurbolinksAdapter from 'vue-turbolinks';
+import Vue from "vue"
+import system from "lux-design-system";
+// import "lux-design-system/dist/system/system.css";
+// import "lux-design-system/dist/system/tokens/tokens.scss";
+// import "../stylesheets/application"
+
+import "popper"
+import "bootstrap"
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs4';
+import BatchForm from "batch_form"
+// require("@nathanvda/cocoon");
+
+// Rails.start()
+Turbolinks.start()
+// ActiveStorage.start()
+
+window.DataTable = DataTable();
+
+Vue.use(TurbolinksAdapter);
+Vue.use(system)
+// create the LUX app and mount it to wrappers with class="lux"
+var loadPage = () => {
+  var elements = document.getElementsByClassName("lux")
+  for (var i = 0; i < elements.length; i++) {
+    new Vue({
+      el: elements[i]
+    })
+  }
+  new BatchForm()
+}
+document.addEventListener("DOMContentLoaded", () => {
+  $('.datatable').DataTable({
+    "searching": false,
+    "paging": false,
+    "info": false
+  })
+})
+document.addEventListener("turbolinks:load", loadPage)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "batch_form", preload: true
+pin "vue", to: "https://ga.jspm.io/npm:vue@2.6.12/dist/vue.runtime.common.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process-production.js"
+pin "vue-turbolinks", to: "https://ga.jspm.io/npm:vue-turbolinks@2.2.2/index.js"
+pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
+pin "lux-design-system", to: "https://ga.jspm.io/npm:lux-design-system@2.17.0/dist/system/system.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "datatables.net-bs4", to: "https://ga.jspm.io/npm:datatables.net-bs4@1.10.25/js/dataTables.bootstrap4.js"
+pin "datatables.net", to: "https://ga.jspm.io/npm:datatables.net@1.10.25/js/jquery.dataTables.js"
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
