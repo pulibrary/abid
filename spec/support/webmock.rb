5b6ae6b16ca90e6d270eb694a97cd7e2f2340df2
Anna Headley <anna.headley@gmail.com>
2023-08-04 16:24:42 -0400

    Update webdrivers past chromedriver 115 errors
    and configure webmock to allow googles new chromedriver distribution
    urls

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 WebMock.disable_net_connect!(
   allow_localhost: true,
-  allow: "chromedriver.storage.googleapis.com"
+  allow: [
+    "chromedriver.storage.googleapis.com",
+    "googlechromelabs.github.io",
+    "edgedl.me.gvt1.com"
+  ]
 )
