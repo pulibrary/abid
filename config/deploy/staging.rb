70aa3ad602dd453706bd9fd120d6752e05441bbd
Francis Kayiwa <kayiwa@pobox.com>
2023-08-10 18:27:48 -0400

    switch to ruby 3.1.3 and add new jammy vm

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 server "abid-staging1.princeton.edu", user: "deploy", roles: %w[app db web]
+server "abid-staging2.princeton.edu", user: "deploy", roles: %w[app db web]
