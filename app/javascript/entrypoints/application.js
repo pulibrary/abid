5736a3e6627ac17ae414153c1223df62d961f9c2
Eliot Jordan <eliotj@princeton.edu>
2024-04-03 15:37:49 -0500

    Upgrade lux and vue

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,14 +1,13 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import Vue from "vue/dist/vue.esm";
-import system from "lux-design-system";
-import "lux-design-system/dist/system/system.css";
-import "lux-design-system/dist/system/tokens/tokens.scss";
 import "bootstrap"
 import "@/stylesheets/application.scss"
 import $ from 'jquery';
 import 'datatables.net-bs4';
+import {createApp} from "vue";
+import lux from "lux-design-system";
+import "lux-design-system/dist/style.css";
 import BatchForm from "@/batch_form"
 import TurbolinksAdapter from 'vue-turbolinks';
 import "cocoon-js-vanilla";
@@ -17,15 +16,14 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-Vue.use(TurbolinksAdapter);
-Vue.use(system)
+const app = createApp({});
+const createMyApp = () => createApp(app);
+
 // create the LUX app and mount it to wrappers with class="lux"
 var loadPage = () => {
-  var elements = document.getElementsByClassName("lux")
-  for (var i = 0; i < elements.length; i++) {
-    new Vue({
-      el: elements[i]
-    })
+  const elements = document.getElementsByClassName('lux')
+  for(let i = 0; i < elements.length; i++){
+    createMyApp().use(lux).use(TurbolinksAdapter).mount(elements[i]);
   }
   new BatchForm()
 }
@@ -37,4 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
     "info": false
   })
 })
-document.addEventListener("turbolinks:load", loadPage)
+document.addEventListener("turbolinks:load", loadPage, { once: true })
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    vue()
   ],
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-bundler',
+    },
+  },
   build: {
     sourcemap: true
   }
