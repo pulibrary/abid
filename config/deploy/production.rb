33011ab278641d2eba8b0bd695ad1c2269a472a0
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-07-29 09:51:32 -0400

    Remove capistrano and deploy files
    They're confusing since those boxes don't exist anymore
    
    closes #189

--- config/deploy/production.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-server "abid-prod1.princeton.edu", user: "deploy", roles: %w[app db web]
-server "abid-prod2.princeton.edu", user: "deploy", roles: %w[app web]
--- config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-server "abid-staging2.lib.princeton.edu", user: "deploy", roles: %w[app db web]
